28d577d169e563e8699ac875950672ce02fc1722
bornittah <bornittah@gmail.com>
2022-05-31 10:44:35 +0300

    Implement js classes

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
 # awesomebooks-v2
+
+Awesome books is a simple website that displays a list of books and allows you to add and remove books from that list. The aim of building this application is to learn how to manage data using JavaScript.
+
+# Deployment
+Click [Awesomebooks](https://bornittah.github.io/awesomebooks/) to checkout the online version
+
+# Getting started
+#### Step 1
+    - Navigate to the local folder and clone the repository from the github 
+    - Run "git clone https://github.com/Bornittah/awesomebooks.git"
+#### Step 2
+    - Navigate to the cloned project
+    - open the index.html file in your browser
+
+# Built With
+- Languages - HTML, CSS and Javascript
+- Frameworks - None
+- Technologies - Web Technology
+- Other tools - HTML & CSS Linters
+
+# Contributing
+Feel free to check out the [Issues page](https://github.com/Bornittah/awesomebooks/issues)
+
+# Show your support
+Give a star if you like this project
+
+# Acknowledgement
+ [Microverse](https://www.microverse.org/?grsf=i6yi2m)
+ 
+# License
+This project is MIT licensed.
+
+# Author
+1. [Agasha Bornittah](https://github.com/Bornittah)
---
+++ js/index.js
@@ -0,0 +1,107 @@
+let listSection = document.querySelector('.booklist');
+let addSection = document.querySelector('.addnew');
+let contactSection = document.querySelector('.contacts');
+
+
+let listMenuLink = document.querySelector('#bookslist');
+let addMenuLink = document.querySelector('#addbooks');
+let contactMenuLink = document.querySelector('#contact');
+
+document.querySelectorAll('.navbar').forEach((link)=>{
+  link.addEventListener('click', ()=>{
+    listMenuLink.addEventListener('click', ()=>{
+      listSection.style.display='block';
+      addSection.style.display='none';
+      contactSection.style.display='none';
+    });
+    
+    addMenuLink.addEventListener('click', ()=>{
+        listSection.style.display='none';
+        addSection.style.display='block';
+        contactSection.style.display='none';
+      
+    });
+    
+    contactMenuLink.addEventListener('click', ()=>{
+      listSection.style.display='none';
+      addSection.style.display='none';
+      contactSection.style.display='block';
+    });
+});
+});
+
+// storage
+  class Books {
+    constructor(title, author){
+      this.title=title;
+      this.author=author;
+    }
+
+    addbook(){
+      let book = {
+        'title': this.title.value,
+        'author': this.author.value
+      };
+    console.log(book)
+      let books = [];
+      if (JSON.parse(localStorage.getItem('booklist')) === null) {
+        books.push(book);
+        localStorage.setItem('booklist', JSON.stringify(books));
+      } 
+      else {
+        let newbooks = JSON.parse(localStorage.getItem('booklist'));
+        newbooks.push(book);
+        localStorage.setItem('booklist', JSON.stringify(newbooks));
+      }
+    }
+
+    fetchbooks(){
+      let booklist = document.querySelector('.list');
+      let data = JSON.parse(localStorage.getItem('booklist'));
+      
+        let str='';
+        if (JSON.parse(localStorage.getItem('booklist')) === null || data.length===0) {
+          str=`<li class="list-item">No book stored!</li>`;
+        }
+        else{
+          for(let obj of data){
+          str+=`<li class="list-item">
+          <p>${obj.title} by ${obj.author}</p>
+          <a href="" class="remove-btn" id="remove-book">Remove</a>
+        </li>`
+        }
+        }
+        booklist.innerHTML=str;
+
+    }
+
+    removebook(id){
+      let data = JSON.parse(localStorage.getItem('booklist'));
+      let selectedbook=data[id];
+      let filteredBooks=data.filter((item)=>{
+        return item !== selectedbook;
+      });
+      localStorage.setItem('booklist', JSON.stringify(filteredBooks));
+      let newData=JSON.parse(localStorage.getItem('booklist'));
+      data = newData;
+    }
+  }
+
+  let titleInput = document.querySelector('#title-input');
+  let authorInput = document.querySelector('#author-input');
+  
+  let bk = new Books(titleInput,authorInput);
+
+  document.getElementById('form').addEventListener('submit', (e)=>{
+  bk.addbook();
+  });
+
+  bk.fetchbooks();
+  
+  document.querySelectorAll('#remove-book').forEach((button, id)=>{
+    button.addEventListener('click', (e)=>{
+    bk.removebook(id);
+  })
+});
+
+
