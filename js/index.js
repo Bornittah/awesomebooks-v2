8e55f2074ecea3721be806751243f159eaff51c6
tingamapuro04 <82979239+tingamapuro04@users.noreply.github.com>
2022-06-01 16:53:36 +0300

    Linter corrections

--- js/index.js
+++ js/index.js
@@ -1,116 +1,107 @@
-let listSection = document.querySelector('.booklist');
-let addSection = document.querySelector('.addnew');
-let contactSection = document.querySelector('.contacts');
+const listSection = document.querySelector('.booklist');
+const addSection = document.querySelector('.addnew');
+const contactSection = document.querySelector('.contacts');
 
+const listMenuLink = document.querySelector('#bookslist');
+const addMenuLink = document.querySelector('#addbooks');
+const contactMenuLink = document.querySelector('#contact');
 
-let listMenuLink = document.querySelector('#bookslist');
-let addMenuLink = document.querySelector('#addbooks');
-let contactMenuLink = document.querySelector('#contact');
-
-document.querySelectorAll('.navbar').forEach((link)=>{
-  link.addEventListener('click', (e)=>{
+document.querySelectorAll('.navbar').forEach((link) => {
+  link.addEventListener('click', (e) => {
     e.preventDefault();
-    listMenuLink.addEventListener('click', ()=>{
-      listSection.style.display='block';
-      addSection.style.display='none';
-      contactSection.style.display='none';
+    listMenuLink.addEventListener('click', () => {
+      listSection.style.display = 'block';
+      addSection.style.display = 'none';
+      contactSection.style.display = 'none';
     });
-    
-    addMenuLink.addEventListener('click', ()=>{
-        listSection.style.display='none';
-        addSection.style.display='block';
-        contactSection.style.display='none';
-      
+
+    addMenuLink.addEventListener('click', () => {
+      listSection.style.display = 'none';
+      addSection.style.display = 'block';
+      contactSection.style.display = 'none';
     });
-    
-    contactMenuLink.addEventListener('click', ()=>{
-      listSection.style.display='none';
-      addSection.style.display='none';
-      contactSection.style.display='block';
+
+    contactMenuLink.addEventListener('click', () => {
+      listSection.style.display = 'none';
+      addSection.style.display = 'none';
+      contactSection.style.display = 'block';
     });
-});
+  });
 });
 
 // storage
-let titleInput = document.querySelector('#title-input');
-let authorInput = document.querySelector('#author-input');
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
 class Books {
-    constructor(title, author){
-      this.title=title;
-      this.author=author;
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 
-    clearfields() {
-      titleInput.value = '';
-      authorInput.value = '';
-    }
+  static clearfields() {
+    document.querySelector('#title-input').value = '';
+    document.querySelector('#author-input').value = '';
+  }
 
-    addbook(){
-      let book = {
-        'title': this.title.value,
-        'author': this.author.value
-      };
+  addbook() {
+    const book = {
+      title: this.title.value,
+      author: this.author.value,
+    };
 
-      let books = [];
-      if (JSON.parse(localStorage.getItem('booklist')) === null) {
-        books.push(book);
-        localStorage.setItem('booklist', JSON.stringify(books));
-      } 
-      else {
-        let newbooks = JSON.parse(localStorage.getItem('booklist'));
-        newbooks.push(book);
-        localStorage.setItem('booklist', JSON.stringify(newbooks));
-      }
-    }
-
-    fetchbooks(){
-      let booklist = document.querySelector('.list');
-      let data = JSON.parse(localStorage.getItem('booklist'));
-      
-        let str='';
-        if (JSON.parse(localStorage.getItem('booklist')) === null || data.length===0) {
-          str=`<li class="list-item">No book stored!</li>`;
-        }
-        else{
-          for(let obj of data){
-          str+=`<li class="list-item">
-          <p>${obj.title} by ${obj.author}</p>
-          <a href="" class="remove-btn" id="remove-book">Remove</a>
-        </li>`
-        }
-        }
-        booklist.innerHTML=str;
-
-    }
-
-    removebook(id){
-      let data = JSON.parse(localStorage.getItem('booklist'));
-      let selectedbook=data[id];
-      let filteredBooks=data.filter((item)=>{
-        return item !== selectedbook;
-      });
-      localStorage.setItem('booklist', JSON.stringify(filteredBooks));
-      let newData=JSON.parse(localStorage.getItem('booklist'));
-      data = newData;
+    const books = [];
+    if (JSON.parse(localStorage.getItem('booklist')) === null) {
+      books.push(book);
+      localStorage.setItem('booklist', JSON.stringify(books));
+    } else {
+      const newbooks = JSON.parse(localStorage.getItem('booklist'));
+      newbooks.push(book);
+      localStorage.setItem('booklist', JSON.stringify(newbooks));
     }
   }
 
-  let bk = new Books(titleInput,authorInput);
+  static fetchbooks() {
+    const booklist = document.querySelector('.list');
+    const data = JSON.parse(localStorage.getItem('booklist'));
 
-  document.getElementById('form').addEventListener('submit', (e)=>{
-    e.preventDefault();
-    bk.addbook();
-    bk.clearfields();
-    bk.fetchbooks();
-  });
+    let str = '';
+    if (JSON.parse(localStorage.getItem('booklist')) === null || data.length === 0) {
+      str = '<li class="list-item">No book stored!</li>';
+    } else {
+      for (const obj of data) {
+        str += `<li class="list-item">
+          <p>${obj.title} by ${obj.author}</p>
+          <a href="" class="remove-btn" id="remove-book">Remove</a>
+        </li>`;
+      }
+    }
+    booklist.innerHTML = str;
+  }
 
+  static removebook(id) {
+    let data = JSON.parse(localStorage.getItem('booklist'));
+    const selectedbook = data[id];
+    const filteredBooks = data.filter((item) => item !== selectedbook);
+    localStorage.setItem('booklist', JSON.stringify(filteredBooks));
+    const newData = JSON.parse(localStorage.getItem('booklist'));
+    data = newData;
+  }
+}
+
+const bk = new Books(titleInput, authorInput);
+
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  bk.addbook();
+  bk.clearfields();
   bk.fetchbooks();
-  
-  document.querySelectorAll('#remove-book').forEach((button, id)=>{
-    button.addEventListener('click', (e)=>{
-    bk.removebook(id);
-    bk.fetchbooks();
-  })
 });
 
+bk.fetchbooks();
 
+document.querySelectorAll('#remove-book').forEach((button, id) => {
+  button.addEventListener('click', () => {
+    bk.removebook(id);
+    bk.fetchbooks();
+  });
+});
